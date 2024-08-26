0a37e8ab665a08a682ed8861fbba10e04fb724bf
Bayan-Daleel <bayan.dl96@gmail.com>
2024-08-26 16:14:39 -0600

    first commit

--- animate.js
+++ animate.js
@@ -1,16 +1,23 @@
 const bodyElm=document.querySelector("body");
-bodyElm.addEventListener("mousemove",(event)=>{
-   const spanX=event.offsetX;
-   const spanY=event.offsetY;
-   const spanElm=document.createElement("span");
-   spanElm.style.left=spanX+"px";
-   spanElm.style.top=spanY+"px";
-   const size=Math.random()*100;
-   spanElm.style.width=size+"px";
-   spanElm.style.height=size+"px";
-   bodyElm.appendChild(spanElm);
-    setTimeout(()=>{
-        spanElm.remove(); 
-    },3000);
-   
+const inputElm=document.querySelector(".input");
+
+inputElm.checked =JSON.parse(localStorage.getItem("mode"));
+updatebody();
+
+inputElm.addEventListener("input",()=>{
+    updatebody();
+    updateLocalstorge();
+    
 });
+function updatebody(){
+   if(inputElm.checked){
+     bodyElm.style.background="black";
+   }
+   else{
+    bodyElm.style.background="white";
+   }
+}
+
+function updateLocalstorge(){
+    localStorage.setItem("mode",JSON.stringify(inputElm.checked));
+}
